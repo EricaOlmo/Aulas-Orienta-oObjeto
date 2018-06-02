00cf886e2a9e0a0bd65a3173953e84efeca504e7
EricaOlmo <30755306+EricaOlmo@users.noreply.github.com>
2018-06-02 13:26:24 -0300

    Exercicios reposicao greve caminhoneiros

---
+++ Aula290518-LivrosGreve/AulaLivros/src/br/com/Borges/LivroDeLivrariaV2.java
@@ -0,0 +1,23 @@
+
+package br.com.Borges;
+
+public class LivroDeLivrariaV2 {
+   private double valor;
+    private double codigoBarra;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getCodigoBarra() {
+        return codigoBarra;
+    }
+
+    public void setCodigoBarra(double codigoBarra) {
+        this.codigoBarra = codigoBarra;
+    }  
+}
